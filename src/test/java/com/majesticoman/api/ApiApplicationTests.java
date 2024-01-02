e4cad55d4136d66511a5d08ce96c909b5f6224b9
alhabsi16 <134998441+alhabsi16@users.noreply.github.com>
2024-01-02 11:13:08 +0400

    create an empty spring boot 3 project (#1)

---
+++ src/test/java/com/majesticoman/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.majesticoman.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
