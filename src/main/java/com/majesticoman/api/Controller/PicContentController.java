2a9e9747f34a0f329aa0843aec6fdd588c2f4524
SalimAlsaadi <al-saadis-s-s@hotmail.com>
2024-01-03 12:07:16 +0400

    fixed duplicates

--- src/main/java/com/majesticoman/api/Controller/PicContentController.java
+++ src/main/java/com/majesticoman/api/Controller/PicContentController.java
@@ -2,6 +2,7 @@ package com.majesticoman.api.Controller;
 
 
 import com.majesticoman.api.Models.PictureInfo;
+import com.majesticoman.api.Repository.PicInfoRepository;
 import com.majesticoman.api.Services.PicContentService;
 import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,28 +19,27 @@ public class PicContentController {
 
     @Autowired
     public PicContentService picContentService;
+
+    @Autowired
+    public PicInfoRepository picInfoRepository;
     @GetMapping(path = "/{ID}")
-    public ResponseEntity<Byte[]> getPicture(){return null;}
+    public ResponseEntity<Byte[]> getPicture(){
+
+
+        return null;}
 
     @PostMapping
     public ResponseEntity<String> addNewPicture(@RequestParam MultipartFile file, String picName) throws IOException {
-        // Save the individual file to a specific directory
-        File fileDestination = new File("./data/" + picName + ".jpg");
-        FileUtils.copyInputStreamToFile(file.getInputStream(), fileDestination);
-
         // Save the file to another directory (C:\\Java_Practices\\majestic-oman-api\\pictures)
         File fileDir = new File("C:\\Java_Practices\\majestic-oman-api\\pictures");
         File fileDestinationInDir = new File(fileDir, picName + ".jpg");
         FileUtils.copyInputStreamToFile(file.getInputStream(), fileDestinationInDir);
 
 
-        PictureInfo pictureInfo = new PictureInfo(0, picName, null, fileDir.toString());
+        PictureInfo pictureInfo = new PictureInfo(0, picName, null, fileDir.getPath());
         picContentService.createNewPic(pictureInfo);
         return ResponseEntity.ok("New Picture Saved");
     }
-
-
-
 
 
 
--- src/main/java/com/majesticoman/api/Services/PicContentService.java
+++ src/main/java/com/majesticoman/api/Services/PicContentService.java
@@ -1,9 +1,14 @@
 package com.majesticoman.api.Services;
 
+import com.majesticoman.api.Controller.PicContentController;
 import com.majesticoman.api.Models.PictureInfo;
 import com.majesticoman.api.Repository.PicInfoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.Optional;
 
 @Service
 public class PicContentService {
@@ -11,8 +16,11 @@ public class PicContentService {
     @Autowired
     public PicInfoRepository picInfoRepository;
 
+
     public PictureInfo createNewPic(PictureInfo pictureInfo){
         picInfoRepository.save(pictureInfo);
         return pictureInfo;
     }
+
+
 }
